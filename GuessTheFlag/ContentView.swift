d5fb00ff233bbcc49056f0f16c557c827fb88a59
Shun702 <138392252+Shun702@users.noreply.github.com>
2023-07-23 20:54:33 +0100

    animation added
    spins horizontally when correct flag is selected, and blurs the other flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,8 +15,9 @@ struct ContentView: View {
     @State private var correctScore = 0
     @State private var gameNum = 0
     @State private var endgame = false
-
     
+    @State private var animationAmount = 0.0
+    @State private var correctflag = false
     var body: some View {
         ZStack {
             RadialGradient(stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3), .init(color: Color(red: 0.76, green: 0.15, blue:0.26), location: 0.3),], center: .top, startRadius: 200, endRadius: 700).ignoresSafeArea()
@@ -29,76 +30,113 @@ struct ContentView: View {
                         Text(countries[correctAnswer]).font(.largeTitle.weight(.semibold))
                     }
                     ForEach(0..<3) {
-                        number in Button {
+                        number in Button (action: {
                             flagTapped(number)
-                        } label: {
-                            Image(countries[number]).renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
+                            withAnimation{
+                                animationAmount = 360.0
+                            }
+                        }) {
+                            if correctflag{
+                                if number == correctAnswer{
+                                    withAnimation{
+                                        Image(countries[number]).renderingMode(.original)
+                                            .something()
+                                            .rotation3DEffect(Angle(degrees: animationAmount), axis: (x: 0, y: 1, z: 0))
+                                    }
+                                }else {
+                                    Image(self.countries[number])
+                                        .renderingMode(.original)
+                                        .something()
+                                        .opacity(0.75)
+                                }
+                            } else {
+                                Image(self.countries[number])
+                                    .renderingMode(.original)
+                                    .something()
+                            }
+                            
                         }
                     }
-                }.frame(maxWidth: .infinity).padding(.vertical, 20).background(.regularMaterial).clipShape(RoundedRectangle(cornerRadius: 20))
-                Spacer()
-                Spacer()
-                HStack {
-                    Text("Socre: ").foregroundColor(.white).font(.title.bold())
-                    Text(correctScore, format: .number).foregroundColor(.white).font(.title.bold())
+                    
                 }
-                Spacer()
-            }.padding()
-            .alert(scoreTitle, isPresented: $showingScore) {
-                Button("Continue", action: askQuestion)
-            } message: {
-                Text("Your score is \(correctScore)")
-            }.alert(scoreTitle, isPresented: $endgame) {
-                Button("New Game", action: createNewGame)
-            } message: {
-                Text("End of the game. You got \(correctScore) / 8.")
+                    Spacer()
+                    Spacer()
+                    HStack {
+                        Text("Socre: ").foregroundColor(.white).font(.title.bold())
+                        Text(correctScore, format: .number).foregroundColor(.white).font(.title.bold())
+                    }
+                    Spacer()
+                }.padding()
+                    .alert(scoreTitle, isPresented: $showingScore) {
+                        Button("Continue", action: askQuestion)
+                    } message: {
+                        Text("Your score is \(correctScore)")
+                    }.alert(scoreTitle, isPresented: $endgame) {
+                        Button("New Game", action: createNewGame)
+                    } message: {
+                        Text("End of the game. You got \(correctScore) / 8.")
+                    }
+            }
+        }
+            
+            func flagTapped(_ number: Int) {
+                if number == correctAnswer {
+                    addScore()
+                    scoreTitle = "Correct"
+                    correctflag = true
+                    
+                }else {
+                    scoreTitle = "Wrong! That is \(countries[number])."
+                }
+                
+                showingScore = true
+                
+                numOfGame()
             }
             
-        }
-    }
-    
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer {
-            addScore()
-            scoreTitle = "Correct"
+            func askQuestion() {
+                countries.shuffle()
+                correctAnswer = Int.random(in: 0...2)
+                correctflag = false
+                animationAmount = 0.0
+            }
+            func addScore() {
+                correctScore = correctScore + 1
+            }
             
-        }else {
-            scoreTitle = "Wrong! That is \(countries[number])."
+            //adds 1 after each number of game and checks if the number reaches 8
+            func numOfGame() {
+                gameNum = gameNum + 1
+                if gameNum >= 8 {
+                    endgame = true
+                }
+            }
+            
+            func createNewGame(){
+                gameNum = 0
+                correctScore = 0
+                endgame = false
+                askQuestion()
+            }
         }
-        
-        showingScore = true
-        numOfGame()
-    }
     
-    func askQuestion() {
-            countries.shuffle()
-            correctAnswer = Int.random(in: 0...2)
-        }
-    func addScore() {
-        correctScore = correctScore + 1
-    }
-    
-    //adds 1 after each number of game and checks if the number reaches 8
-    func numOfGame() {
-        gameNum = gameNum + 1
-        if gameNum >= 8 {
-            endgame = true
-        }
-    }
-    
-    func createNewGame(){
-        gameNum = 0
-        correctScore = 0
-        endgame = false
-        askQuestion()
+struct FlagImage: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .clipShape(Capsule())
+            .shadow(radius: 5)
         
     }
-    
-    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+extension View {
+    func something() -> some View{
+        self.modifier(FlagImage())
     }
 }
+    
+struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
